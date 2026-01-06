f90a1628f4842707768972c2de5c65b354d4e619
joacc2 <joaquin.libera2@gmail.com>
2026-01-06 19:03:56 -0300

    primer commit

---
+++ backend/GestionSocios.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore; // <--- NUEVO USING
+using Microsoft.EntityFrameworkCore;
+using GestionSocios.Api.Models;
+
+namespace GestionSocios.Api.Data
+{
+ 
+    public class ApplicationDbContext : IdentityDbContext
+    {
+    
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Socio> Socios { get; set; }
+    }
+}
